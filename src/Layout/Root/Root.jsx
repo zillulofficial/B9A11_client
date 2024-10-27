c431cf0116b1cbacc94b5db35abea69bd9caaa3b
zillulofficial <zillul.hakim100@gmail.com>
2024-10-27 22:21:56 +0600

    navbar added

--- src/Layout/Root/Root.jsx
+++ src/Layout/Root/Root.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import Navbar from '../Navbar/Navbar';
 
 const Root = () => {
     return (
         <div>
             {/* Navbar */}
-            {/* <Navbar></Navbar> */}
+            <Navbar></Navbar>
 
             {/* Outlet */}
             <div className="min-h-[calc(100vh-343px)]">
